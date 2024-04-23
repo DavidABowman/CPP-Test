ee377ee2ae23c33420c99c3a36211e95b51b3225
DavidABowman <2davidbowman@gmail.com>
2024-04-23 10:24:36 +0100

    Create WeirdAlgorithm-OddEven.cpp

---
+++ WeirdAlgorithm-OddEven.cpp
@@ -0,0 +1,21 @@
+//Weird Algorithm
+//Algorithm takes postivive integer n as an input. If n = even, divide by 2. 
+// If n is odd, mmultiply by 3 and add 1.
+//Repeat until n is 1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ll n:
+    cin >> n;
+    while(n>1) {
+        if(n%2==0)
+            n/=2:
+        else
+            n=3*n+1:
+        cout << " " << n:
+    }
+}
